c9427d3aea29f58bd91bbc02e17c627fa440d77e
Jburt4 <jburt44@gmail.com>
2018-02-02 13:56:52 -0500

    big

--- BigGame.py
+++ BigGame.py
@@ -1,2 +1,23 @@
 #STRIKEGROUPGAME
+#https://pythonprogramming.net/pygame-python-3-part-1-intro/
+import pygame
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((1920,1080))
+
+clock = pygame.time.Clock()
+
+gameover = FALSE
+
+while not gameover:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameover = True
+
+		print(event)
+
+	pygame.display.update()
+	clock.tick(60)
 
