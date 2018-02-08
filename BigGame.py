c0d73ac289ddfb2a03465fa277ca8f5756dc0f81
Jburt4 <jburt44@gmail.com>
2018-02-08 12:27:02 -0500

    big game start

--- BigGame.py
+++ BigGame.py
@@ -4,20 +4,37 @@ import pygame
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((1920,1080))
+height = 800
+width = 1600
+gameDisplay = pygame.display.set_mode((width,height))
+black =(0,0,0)
+blue = (30,64,150)
+white=(255,255,255)
+
+img = pygame.image.load('battleship.png')
+newimg = pygame.transform.scale(img,(275,75))
+def plane(x,y):
+	gameDisplay.blit(newimg,(x,y))
+
+
+x = 600
+y = 300
 
 clock = pygame.time.Clock()
 
-gameover = FALSE
+over = False
 
-while not gameover:
+while not over:
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			gameover = True
+			over = True
 
-		print(event)
+	gameDisplay.fill(blue)
+	plane(x,y)
 
 	pygame.display.update()
 	clock.tick(60)
 
+pygame.quit()
+quit()
